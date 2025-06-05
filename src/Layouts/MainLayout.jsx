aa165cfff495122a0752108c898d4520d735d2e7
tushar <iktushar2020@gmail.com>
2025-06-05 13:01:13 +0600

    cursor animation added

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../Shared/Navbar/Navbar';
 import Footer from '../Shared/Footer/Footer';
+import SmoothFollower from '../Shared/Cursor/SmoothFollower';
 
 const MainLayout = () => {
     return (
         <div>
+            <SmoothFollower></SmoothFollower>
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
