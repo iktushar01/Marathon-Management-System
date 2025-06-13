c8fe118190392037a699f6b09f04615874a9a94b
tushar <iktushar2020@gmail.com>
2025-06-13 16:00:37 +0600

    lotiiefile loading animation added

--- src/Shared/Loading/Loading.jsx
+++ src/Shared/Loading/Loading.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import Lottie from 'lottie-react';
+import runningAnimation from '../../assets/loading.json';
 const Loading = () => {
     return (
         <div className="fixed inset-0 bg-gray-900 bg-opacity-75 flex items-center justify-center z-50">
             <div className="text-center">
-                <span className="loading loading-spinner loading-lg text-primary"></span>
-                <p className="mt-4 text-gray-300 font-medium">Loading...</p>
+                <div className="w-32 h-32 mx-auto">
+                    <Lottie 
+                        animationData={runningAnimation} 
+                        loop={true} 
+                        autoplay={true}
+                    />
+                </div>
             </div>
         </div>
     );
